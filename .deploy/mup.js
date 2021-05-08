98fd1b583d3ca401c863e936629069dccaaf6c0a
zafuzi <zacharyfoutz@gmail.com>
2021-05-08 16:40:29 -0700

    fixed login errors, added ssl deploy

--- .deploy/mup.js
+++ .deploy/mup.js
@@ -1,53 +1,51 @@
 module.exports = {
-  servers: {
-    one: {
-      host: '165.232.146.70',
-      username: 'root',
-    }
-  },
+	servers: {
+		one: {
+			// change to your server ip
+			host: '165.232.146.70',
+			username: 'root',
+		}
+	},
 
-  app: {
-    name: 'meteor-micro-server',
-    path: '../',
+	app: {
+		name: 'meteor-micro-server',
+		path: '../',
 
-    servers: {
-      one: {},
-    },
+		servers: {
+			one: {},
+		},
 
-    buildOptions: {
-      serverOnly: true,
-    },
+		buildOptions: {
+			serverOnly: true,
+		},
 
-    env: {
-      ROOT_URL: 'http://165.232.146.70',
-      MONGO_URL: 'mongodb://mongodb/meteor',
-      MONGO_OPLOG_URL: 'mongodb://mongodb/local',
-    },
+		env: {
+			// server url 
+			ROOT_URL: 'https://www.moistboat.com',
+			MONGO_URL: 'mongodb://mongodb/meteor',
+			MONGO_OPLOG_URL: 'mongodb://mongodb/local',
+		},
 
-    docker: {
-      image: 'abernix/meteord:node-12-base',
-    },
+		docker: {
+			image: 'abernix/meteord:node-12-base',
+		},
 
-    enableUploadProgressBar: true
-  },
+		enableUploadProgressBar: true
+	},
 
-  mongo: {
-    version: '3.4.1',
-    servers: {
-      one: {}
-    }
-  },
+	mongo: {
+		version: '3.4.1',
+		servers: {
+		one: {}
+		}
+	},
 
-  // (Optional)
-  // Use the proxy to setup ssl or to route requests to the correct
-  // app when there are several apps
-
-  // proxy: {
-  //   domains: 'mywebsite.com,www.mywebsite.com',
-
-  //   ssl: {
-  //     // Enable Let's Encrypt
-  //     letsEncryptEmail: 'email@domain.com'
-  //   }
-  // }
+	proxy: {
+		// change this to your domain
+		domains: 'moistboat.com,www.moistboat.com',
+		// and your email
+		ssl: {
+			letsEncryptEmail: 'zacharyfoutz@gmail.com'	
+		}
+	}
 };
