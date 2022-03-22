bc8df2e3e8fd1e1316fec9fccd6a7efeb3d803dc
Zafuzi <zacharyfoutz@gmail.com>
2022-03-22 10:38:44 -0700

    * updated default deploy

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-DEPLOY_HOSTNAME=us-east-1.galaxy-deploy.meteor.com meteor deploy --settings settings.json blades.meteorapp.com
+DEPLOY_HOSTNAME=us-east-1.galaxy-deploy.meteor.com meteor deploy --settings settings.json meteormicroserver.meteorapp.com
