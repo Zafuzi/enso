3d69c5a39d261c357e5e7efe3345ae46a11309d6
Zafuzi <zacharyfoutz@gmail.com>
2022-07-13 00:28:31 -0700

    * added a theme switcher

--- client/pages/layout.js
+++ client/pages/layout.js
@@ -4,3 +4,44 @@ Meteor.call("ping", function(error, result)
 {
 	console.log(result);
 });
+
+const html = document.querySelector('html');
+html.dataset.theme = `theme-light`;
+
+function switchTheme(theme) {
+	if(theme)
+	{
+		html.dataset.theme = `theme-${theme}`;
+		return;
+	}
+	
+	if(html.dataset.theme === "theme-light")
+	{
+		html.dataset.theme = `theme-dark`;
+		return;
+	}
+	
+	html.dataset.theme = `theme-light`;
+}
+
+Template.layout.onCreated(function()
+{
+	this.theme = new ReactiveVar(html.dataset.theme);	
+});
+
+Template.layout.helpers(
+{
+	isDarkTheme: function()
+	{
+		return Template.instance().theme.get() === "theme-dark";
+	}
+});
+
+Template.layout.events(
+{
+	"click .appHeader__switchTheme": function(event, instance)
+	{
+		switchTheme();
+		instance.theme.set(html.dataset.theme);
+	}
+});
