f927cd02e530d96c5d7b4f18c865b071f064e9b9
Zachary Foutz <zachary@boomlearning.com>
2023-03-23 15:23:15 -0700

    * made app set title to app_version when loaded

--- client/pages/layout.js
+++ client/pages/layout.js
@@ -1,4 +1,12 @@
+import {APP_VERSION} from "../../lib/lib";
+
 Meteor.call("ping", function(error, result)
 {
 	console.log(result);
 });
+
+Template.layout.onCreated(function()
+{
+    const currentRoute = Router.current().route.getName();
+    document.title = `${document.title} - ${APP_VERSION}`;
+})
