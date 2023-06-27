2fc0cffeec99418ac1d309d608537ab910cd61c3
Zafuzi <7799288+Zafuzi@users.noreply.github.com>
2023-06-26 17:12:10 -0700

    cleaned up styles for header and moved alerts to their own file

--- client/pages/home/home.ts
+++ client/pages/home/home.ts
@@ -1,67 +1,12 @@
 import {ReactiveVar} from "meteor/reactive-var";
-import Toastify from "toastify-js";
+import {Alert} from "../../alerts";
 
 Template.home.onCreated(function()
 {
 	const instance = this;
 
 	instance.count = new ReactiveVar(0);
-})
-
-namespace Alert {
-	const show = function(message: string, options?: any, style?: any)
-	{
-		Toastify({
-			text: message,
-			close: true,
-			gravity: "top", // `top` or `bottom`
-			position: "center",
-			stopOnFocus: true, // Prevents dismissing of toast on hover
-			style: {
-				fontSize: "16pt",
-				fontWeight: "600",
-				color: "var(--lighter)",
-				width: "98%",
-				maxWidth: "900px",
-				display: "flex",
-				alignItems: "center",
-				justifyContent: "space-between",
-				borderRadius: "8px",
-				padding: "16px",
-				...style
-			},
-			...options
-		}).showToast();
-	}
-	export function success(message: string, options?: any)
-	{
-		show(message, null, {
-			backgroundColor: "var(--balanced)",
-		});
-	}
-
-	export function error(message: string, options?: any)
-	{
-		show(message, null, {
-			backgroundColor: "var(--assertive)",
-		});
-	}
-
-	export function warning(message: string, options?: any)
-	{
-		show(message, null, {
-			backgroundColor: "var(--energized)",
-			color: "var(--dark)",
-		});
-	}
-
-	export function information(message: string, options?: any)
-	{
-		show(message, null, {
-			backgroundColor: "var(--positive-darker)",
-		});
-	}
-}
+});
 
 Template.home.events(
 {
